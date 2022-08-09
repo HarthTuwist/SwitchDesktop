bb66c4c942dd0d5c1f49fc8abc77e5bf98280d31
Harth Tuwist <harth.tuwist@web.de>
2022-08-09 03:42:39 +0200

    Made it such that Firefox is always opened on middle desktop. Has ugly flashing somewhere else though. Copied Win H Win J functionality over

--- SwitchDesktopWrapper.ahk
+++ SwitchDesktopWrapper.ahk
@@ -1,9 +1,14 @@
+;Set this such that Jumping with Win H and Win J does not make task bar blink in yellow
+#WinActivateForce
+;I am not sure why I am doing this anymore and whether this is necessary
+#InstallKeybdHook
+
+
 ;Setup powershell heavy lifter:
 
 ;"(get-process | ? { $_.processname -eq "powershell" } )| stop-process"  
 ;Process, Close, powershell.exe
 Run, powershell -Command "powershell -WindowStyle hidden -file  C:\SwitchDesktopScripts\SwitchDesktops.ps1"
-#InstallKeybdHook
 
 ;LWin & f::switchDesktopByNumber(1)
 <#f::
@@ -48,13 +53,119 @@ return
 	Send {F19 up} 
 return
 
-<#h::
-	Run, C:\Program Files\Mozilla Firefox\firefox.exe -foreground
+#!c::
+	Send {F20 down}
+	Sleep 30
+	Send {F20 up} 
 return
 
-<#!c::
-	Run, C:\Program Files\Mozilla Firefox\firefox.exe -private-window -foreground
+#!f::
+	Send {F21 down}
+	Sleep 30
+	Send {F21 up} 
 return
-;LWin & c::switchDesktopByNumber(2)
-;LWin & s::switchDesktopByNumber(3)
-;LWin & y::switchDesktopByNumber(4)
+
+;<#h::
+;	Run, C:\Program Files\Mozilla Firefox\firefox.exe -foreground
+;return
+
+;<#!c::
+;	Run, C:\Program Files\Mozilla Firefox\firefox.exe -private-window -foreground
+;return
+
+
+;Jump to the right
+<#H::
+    winget, activeID, ,, A
+    ;wingettitle, testtitle, ahk_id %activeID%
+    wingetpos, activeX, activeY, activeW, activeH, A
+    ;msgBox, %testtitle%,  %activeX%, %activeY%, %activeW%
+    
+    winget, id, list,
+    titles:=""
+    titles.= "active X and W:" activeX "," activeW "`n"
+    targetId := activeID
+    targetXBorder := -16000
+    MinImproment := 200
+    leeway := 100
+    loop, %id%
+    {
+        this_id := id%a_index%
+        wingetpos, X, Y, W, H, ahk_id %this_id%
+        wingettitle, this_title, ahk_id %this_id%
+        
+        ;Autohotkey returns some windows with empty titles, I think the task bars. Anyways, we ignore them
+        if(this_title != "")
+        {
+            ;titles.=a_index ": " this_title ";" this_class ";" X ";" W "," targetXBorder "`n" 
+            if (X + W  > targetXBorder + MinImproment && X + W < activeX + leeway)
+            {
+                targetId = %this_id%
+                targetXBorder := X + W
+
+                ;titles.= "changed `n"
+            }
+        }
+    }
+    wingettitle, target_title, ahk_id %targetId%
+    wingetclass, target_class, ahk_id %targetId%
+    titles.= "`n" target_title "," target_class "`n"
+    ;msgBox, % titles
+
+    if (targetId != activeID) 
+    {
+        WinActivate, ahk_id %targetId%
+    }
+return
+
+;jump to the right
+<#J::
+    winget, activeID, ,, A
+    ;wingettitle, testtitle, ahk_id %activeID%
+    wingetpos, activeX, activeY, activeW, activeH, A
+    ;msgBox, %testtitle%,  %activeX%, %activeY%, %activeW%
+    
+    winget, id, list,
+    titles:=""
+    titles.= "active X and W:" activeX "," activeW "`n"
+    targetId := activeID
+    targetXBorder := 16000
+    MinImproment := 200
+    leeway := 100
+    loop, %id%
+    {
+        this_id := id%a_index%
+        wingetpos, X, Y, W, H, ahk_id %this_id%
+        wingettitle, this_title, ahk_id %this_id%
+        
+        ;Autohotkey returns some windows with empty titles, I think the task bars. Anyways, we ignore them
+        if(this_title != "")
+        {
+            ;titles.=a_index ": " this_title ";" this_class ";" X ";" W "," targetXBorder "`n" 
+            if (X < targetXBorder - MinImproment && X > activeX + activeW - leeway)
+            {
+                targetId = %this_id%
+                targetXBorder := X
+
+                ;titles.= "changed `n"
+            }
+        }
+    }
+    wingettitle, target_title, ahk_id %targetId%
+    wingetclass, target_class, ahk_id %targetId%
+    titles.= "`n" target_title "," target_class "`n"
+    ;msgBox, % titles
+
+    if (targetId != activeID) 
+    {
+        WinActivate, ahk_id %targetId%
+    }
+return
+
+#MButton::
+    wingetpos, activeX, activeY, activeW, activeH, A
+    ;MouseMove, activeX + activeW / 2, activeY + activeH / 2, 0
+    ;this can be better than wingetpos according to the documentation???
+    DllCall("SetCursorPos", "int", activeX + activeW / 2, "int", activeY + activeH / 2)
+    ;MouseMove, activeX , activeY , 0
+return
--- SwitchDesktops.ps1
+++ SwitchDesktops.ps1
@@ -2,6 +2,149 @@ $host.ui.RawUI.WindowTitle = "SwitchDesktopsPowershell"
 Import-Module VirtualDesktop
 
 
+#from https://superuser.com/questions/1328345/find-all-window-titles-of-application-through-command-line
+Add-Type  @"
+    using System;
+    using System.Runtime.InteropServices;
+    using System.Text;    
+    public class Win32 {
+        public delegate void ThreadDelegate(IntPtr hWnd, IntPtr lParam);
+        
+        [DllImport("user32.dll")]
+        public static extern bool EnumThreadWindows(int dwThreadId, 
+            ThreadDelegate lpfn, IntPtr lParam);
+        
+        [DllImport("user32.dll", CharSet=CharSet.Auto, SetLastError=true)]
+        public static extern int GetWindowText(IntPtr hwnd, 
+            StringBuilder lpString, int cch);
+        
+        [DllImport("user32.dll", CharSet=CharSet.Auto, SetLastError=true)]
+        public static extern Int32 GetWindowTextLength(IntPtr hWnd);
+
+        [DllImport("user32.dll")]
+        public static extern bool IsIconic(IntPtr hWnd);
+
+        [DllImport("user32.dll")]
+        public static extern bool IsWindowVisible(IntPtr hWnd);
+
+        public static string GetTitle(IntPtr hWnd) {
+            var len = GetWindowTextLength(hWnd);
+            StringBuilder title = new StringBuilder(len + 1);
+            GetWindowText(hWnd, title, title.Capacity);
+            return title.ToString();
+        }
+    }
+"@
+
+
+function WaitFirefoxOpen()
+{
+    param (
+        $Arguments
+    )
+
+   $OriginalWindows = New-Object System.Collections.ArrayList
+    <# Get-Process firefox | Where { $_.MainWindowTitle } | foreach {
+    $_.Threads.ForEach({
+        [void][Win32]::EnumThreadWindows($_.Id, {
+            param($hwnd, $lparam)
+            if ([Win32]::IsIconic($hwnd) -or [Win32]::IsWindowVisible($hwnd)) {
+                $windows.Add([Win32]::GetTitle($hwnd))
+            }}, 0)
+        })} #>
+    $FireFoxAllProcess = Get-Process firefox -ErrorAction SilentlyContinue #do not throw errors if we do not find FF
+    
+    if($FireFoxAllProcess)
+    {
+        $FireFoxAllProcess | Where { $_.MainWindowTitle} | foreach {
+        $_.Threads.ForEach({
+            [void][Win32]::EnumThreadWindows($_.Id, {
+                param($hwnd, $lparam)
+                if ([Win32]::IsIconic($hwnd) -or [Win32]::IsWindowVisible($hwnd)) {
+                    $OriginalWindows.Add($hwnd)
+                }}, 0)
+            })}
+    }
+
+    Start-Process "C:\Program Files\Mozilla Firefox\firefox.exe" -ArgumentList $Arguments
+
+    do
+    {  
+        $NewWindows = New-Object System.Collections.ArrayList
+        
+        #find the new windows exactly like the original ones
+        Get-Process firefox | Where { $_.MainWindowTitle} | foreach {
+        $_.Threads.ForEach({
+        [void][Win32]::EnumThreadWindows($_.Id, {
+            param($hwnd, $lparam)
+            if ([Win32]::IsIconic($hwnd) -or [Win32]::IsWindowVisible($hwnd)) {
+                $NewWindows.Add($hwnd)
+            }}, 0)
+        })}
+
+
+       
+        foreach ($newWind in $NewWindows) {
+            $FoundNewWindow = $true
+            foreach ($orgWind in $OriginalWindows){
+                if($orgWind -eq $newWind)
+                {
+                    $FoundNewWindow = $false
+                }
+            }
+
+            if($FoundNewWindow)
+            {
+                Write-Host FoundWindow $newWind -ForegroundColor White
+
+                $OriginalWindows = @()
+                $OriginalWindows = $OriginalWindows + $NewWindows
+                return $newWind
+            }
+        }
+        Write-Host $NewWindows -ForegroundColor Green
+        Write-Host $OriginalWindows -ForegroundColor DarkRed
+        Start-Sleep -Milliseconds 5
+
+
+    } while($true)
+}
+
+
+    Try { 
+        [void][Window]
+    } Catch {
+    Add-Type @"
+        using System;
+        using System.Runtime.InteropServices;
+        public class Window {
+        [DllImport("user32.dll")]
+        [return: MarshalAs(UnmanagedType.Bool)]
+        public static extern bool GetWindowRect(
+            IntPtr hWnd, out RECT lpRect);
+
+        [DllImport("user32.dll")]
+        [return: MarshalAs(UnmanagedType.Bool)]
+        public extern static bool MoveWindow( 
+            IntPtr handle, int x, int y, int width, int height, bool redraw);
+
+        [DllImport("user32.dll")] 
+        [return: MarshalAs(UnmanagedType.Bool)]
+        public static extern bool ShowWindow(
+            IntPtr handle, int state);
+        }
+        public struct RECT
+        {
+        public int Left;        // x position of upper-left corner
+        public int Top;         // y position of upper-left corner
+        public int Right;       // x position of lower-right corner
+        public int Bottom;      // y position of lower-right corner
+        }
+"@
+    }
+
+
+
 $keyD1  = '0x7C' ## F13
 $keyD2  = '0x7D' ## F14
 $keyD3  = '0x7E' ## F15
@@ -9,6 +152,8 @@ $keyD4  = '0x7F' ## F16
 $keyD5  = '0x80' ## F17
 $keyD6  = '0x81' ## F18
 $keyD7  = '0x82' ## F19
+$keyD8  = '0x83' ## F20
+$keyD9  = '0x84' ## F21
 
 $keyWin = '0x5B' ## Win Key
 
@@ -36,6 +181,8 @@ do
 	$result5 = [PsOneApi.Keyboard]::GetAsyncKeyState($keyD5)
 	$result6 = [PsOneApi.Keyboard]::GetAsyncKeyState($keyD6)
 	$result7 = [PsOneApi.Keyboard]::GetAsyncKeyState($keyD7)
+	$result8 = [PsOneApi.Keyboard]::GetAsyncKeyState($keyD8)
+	$result9 = [PsOneApi.Keyboard]::GetAsyncKeyState($keyD9)
 	$resultWin = [PsOneApi.Keyboard]::GetAsyncKeyState($keyWin)
 
 	$bWinPressed = [bool] (( $resultWin -eq -32767 ) -or ( $resultWin -eq -32768 ))
@@ -97,10 +244,18 @@ do
 	Elseif (([bool] (( $result7 -eq -32767 ) -or ( $result7 -eq -32768 ))) -and $bWinPressed)
 	{
 		$LastKey7 = [Math]::Round((Get-Date).ToFileTimeUTC()/10000)
-
-		#Get-Desktop 0 | Move-ActiveWindow
 	}
-
+	Elseif (([bool] (( $result8 -eq -32767 ) -or ( $result8 -eq -32768 ))) -and $bWinPressed)
+	{
+		$Firefox= WaitFirefoxOpen -Arguments "-new-window" 
+		[Window]::MoveWindow($Firefox, -8, 22, 1936, 1066, $True)
+	}
+	Elseif (([bool] (( $result9 -eq -32767 ) -or ( $result9 -eq -32768 ))) -and $bWinPressed)
+	{
+		$Firefox= WaitFirefoxOpen -Arguments "-private-window" 
+		[Window]::MoveWindow($Firefox, -8, 22, 1936, 1066, $True)
+	}
+	
     Start-Sleep -Milliseconds 15
 
 } while($true)
